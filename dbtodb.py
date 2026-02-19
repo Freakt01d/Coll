82ec44a34bfced72df00be63bf409e007d88ddb6
Vishal S B <79051939+Freakt01d@users.noreply.github.com>
2026-02-19 16:32:25 +0530

    Update dbtodb.py

--- dbtodb.py
+++ dbtodb.py
@@ -10,5 +10,4 @@
 
 "C:\Program Files\Java\jdk-25\bin\java.exe" -Djavax.net.ssl.trustStore="C:\Program Files\Java\jdk-25\lib\security\cacerts" -Djavax.net.ssl.trustStorePassword=changeit -jar agent.jar -jnlpUrl https://cdp-jenkins-paas-rxd-94585.fr.world.socgen/computer/VP1DRXD000DEV1/jenkins-agent.jnlp -secret <your_secret>
 
-"C:\Program Files\Java\jdk-25\bin\java.exe" -jar agent.jar -jnlpUrl https://cdp-jenkins-paas-rxd-94585.fr.world.socgen/computer/VP1DRXD000DEV1/jenkins-agent.jnlp -secret ef8166c450088f0fcd001c5a3b699f7b06657404095df190f0fab0e5a8a80 -noCertificateCheck
-
+"C:\Program Files\Java\jdk-25\bin\java.exe" -jar agent.jar -jnlpUrl https://cdp-jenkins-paas-rxd-94585.fr.world.socgen/computer/VP1DRXD000DEV1/jenkins-agent.jnlp -secret ef8166c450088f0fcd001c5a3b699f7b06657404095df190f0fab0e5a8a80 -cert @C:\sg-root.crt
