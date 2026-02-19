7527b387a2196842cf3074ee02486cc2f9c4cba3
Vishal S B <79051939+Freakt01d@users.noreply.github.com>
2026-02-19 16:16:02 +0530

    Update dbtodb.py

--- dbtodb.py
+++ dbtodb.py
@@ -1,3 +1,7 @@
 "C:\Program Files\Java\jdk-25\bin\keytool" -import -alias sg-root-ca -keystore "C:\Program Files\Java\jdk-25\lib\security\cacerts" -file "SG UniPass Root CA 2016.cer" -storepass changeit
 
 "C:\Program Files\Java\jdk-25\bin\keytool" -import -alias sg-intermediate-ca -keystore "C:\Program Files\Java\jdk-25\lib\security\cacerts" -file "SG-SubCA-SSLForwardProxy-GTS-SGGSC-PRD.cer" -storepass changeit
+
+
+
+"C:\Program Files\Common Files\Oracle\Java\javapath\java.exe" -XshowSettings:properties -version 2>&1 | findstr "java.home"
